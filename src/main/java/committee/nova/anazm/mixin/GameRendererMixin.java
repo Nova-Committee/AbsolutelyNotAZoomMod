38e6b3e080068020ac4b3a6fdec0cd4d6b4cd3d5
MikhailTapio <81358657+MikhailTapio@users.noreply.github.com>
2022-04-04 14:18:31 +0800

    Update GameRendererMixin.java
    Simplification

--- src/main/java/committee/nova/anazm/mixin/GameRendererMixin.java
+++ src/main/java/committee/nova/anazm/mixin/GameRendererMixin.java
@@ -10,14 +10,8 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public abstract class GameRendererMixin {
     @ModifyVariable(method = "getFov", at = @At(value = "RETURN", shift = At.Shift.BEFORE), ordinal = 0)
     private double getFov(double fov) {
-        int d;
-        if (ANAZM.zoomKeyBinding.isPressed()) {
-            d = 9;
-            ANAZM.isZooming = true;
-        } else {
-            d = 1;
-            ANAZM.isZooming = false;
-        }
-        return fov / d;
+        final boolean p = ANAZM.zoomKeyBinding.isPressed();
+        ANAZM.isZooming = p
+        return fov / (p? 9 : 1);
     }
 }
